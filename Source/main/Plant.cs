11a08b5bb74897bdfa8d8c82ab56d80608c259e7
Liam Boyajian <thefirstboyajian@gmail.com>
2026-06-21 23:28:12 -0500

    feature: plant.cs

--- Source/main/Plant.cs
+++ Source/main/Plant.cs
@@ -4,6 +4,21 @@ namespace Main.Source.main;
 
 public partial class Plant : Node
 {
+    //Abstract
+    private float health;
+    private float maxHealth;
+
+    //Definite attributes
+    private double _glucose;
+    private double _h2O;
+    private double _cO2;
+    private double _oxygen;
+    //I think sunlight shouldn't be a field. it isn't a component of the plant and has no reason to be stored.
+
+    //hormones
+    //circadian rhythm
+    //injury
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -11,6 +26,10 @@ public partial class Plant : Node
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
+    {
+    }
+
+    public void Tick()
     {
     }
 }
