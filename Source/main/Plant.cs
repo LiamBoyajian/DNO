a163614966e8ec96051ef3d291eb4a072af6093b
Liam Boyajian <thefirstboyajian@gmail.com>
2026-06-24 15:58:52 -0500

    add: plant dict and enum for fields

--- Source/main/Plant.cs
+++ Source/main/Plant.cs
@@ -1,41 +1,85 @@
+using System;
+using System.Collections.Generic;
 using Godot;
 
 namespace Main.Source.main;
 
 public partial class Plant : Node
 {
-    //Abstract
-    private float _health;
-    private float _maxHealth;
-
-    //Definite attributes
-    private double _glucose;
-    private double _h2O;
-    private double _cO2;
-    private double _oxygen;
-    //I think sunlight shouldn't be a field. it isn't a component of the plant and has no reason to be stored.
-
+    /**
+     * ResourceTypes
+     */
+    public enum Rt
+    {
+    //Abstract:
+    Health,
+    MaxHealth,
+    Chlorophyll,
+    //Definite attributes:
+    Glucose,
+    H2O,
+    Co2,
+    Oxygen,
     //hormones
-    
-    
     //circadian rhythm
-    //injury
-    private float _damagedCells; //maybe add types of cells or damage idk (types of broken proteins.)
+    //injury types:
+    DamagedCells, //maybe add types of cells or damage idk (types of broken proteins.)
+    }
+
+
+    private Godot.Collections.Dictionary<Rt, double> _resources = new()
+    {
+        //Arbitrary base values
+        { Rt.Health, 10.0 },
+        { Rt.MaxHealth, 10.0 },
+        { Rt.Chlorophyll, 100.0 },
+        { Rt.Glucose, 0 },
+        { Rt.H2O, 100.0 },
+        { Rt.Co2, 100.0 },
+        { Rt.Oxygen, 0 },
+
+        { Rt.DamagedCells, 0 },
+
+    };
     
+    
+    //-----------------------------
+    public IReadOnlyDictionary<Rt, double> Resources => _resources;
+    
+    
+    
+    private double _frameSum = 0.0;
+    //-----------------------------
     
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
     }
 
+    
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        Tick(delta);
     }
 
-    public void Tick()
+    public void Tick(double delta)
     {
-        if(GetSunLevel() >= 0.0)_photosynthesize();
+        _frameSum += delta;
+        
+        if (_frameSum < 5.0)
+            return;
+        _frameSum = 0.0;
+        
+        _resources[Rt.H2O] += 25.0;
+        _resources[Rt.H2O] += 50.0;
+        
+        if(GetSunLevel() >= 0.0)
+            _photosynthesize(GetSunLevel());
+
+        
+        Console.Write($"Glucose {_resources[Rt.Glucose]}");
+
     }
 
     /**
@@ -43,7 +87,8 @@ public partial class Plant : Node
      */
     public float GetSunLevel()
     {
-        return 0.0f;
+        var TESTSUM = .8f;
+        return TESTSUM;
     }
     /**
      * ACTIONS: Make changes to a plant's resources
@@ -56,13 +101,28 @@ public partial class Plant : Node
     }
     
     //Photosynthesize: yk what that is
-    private void _photosynthesize(){
-        //exponential but tapering
+    //should soon be exponential 
+    //co2 one to one with water; sun is idk and idc rn
+    private void _photosynthesize(float sunlevel)
+    {
+        const float oxygenByproductRatio = 6.0f;
+        const float waterAndCo2Min = 6f;
+
+        var glucoseGenerated = (int) ((Math.Max(_resources[Rt.H2O], _resources[Rt.Co2]) * sunlevel) / 6.0f); 
+        _resources[Rt.Glucose] += glucoseGenerated;
+        _resources[Rt.Oxygen] += glucoseGenerated * oxygenByproductRatio;
+        _resources[Rt.H2O] -= glucoseGenerated * waterAndCo2Min;
+        _resources[Rt.Co2] -= glucoseGenerated * waterAndCo2Min;
     }
     //Clean: remove a resource permanently
-    private void _clean(){
-    
+    private void _clean(Enum resource){
+        if (resource is not Rt)
+            throw new ArgumentException(resource.ToString() + " is not an Rt.");
+        //stub not sure if I want here yet
+        return; 
     }
+    
+    
     //Store: store specific resources in an organelle or plant structure
     private void _store(){
     
@@ -84,8 +144,13 @@ public partial class Plant : Node
     
     }
     //Cycle: Tell the plant to change its hormonal state
-    private void _cycle(){
-    
+    private void _cycle()
+    {
+
+
     }
+    
+    
+
     
 }
