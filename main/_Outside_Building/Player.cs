381828b7e97b029762c7668f5c657e5e084897ec
Liam Boyajian <thefirstboyajian@gmail.com>
2026-07-20 12:16:40 -0500

    add: walking collision

--- main/_Outside_Building/Player.cs
+++ main/_Outside_Building/Player.cs
@@ -1,4 +1,6 @@
+using System;
 using Godot;
+using Main.main.scripts.scene;
 
 namespace Main.main._Outside_Building;
 
@@ -18,9 +20,14 @@ public partial class Player : AnimatedSprite2D
 {
     [Export] private float _movementSpeed = 50f;
 
+    protected BasicScene ParentScene;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        ParentScene = GetParent() as BasicScene;
+        if (ParentScene == null)
+            GD.PrintErr("Player node not a child of parent scene");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -62,7 +69,8 @@ public partial class Player : AnimatedSprite2D
     {
         if (movementType is MovementTypes.Run or MovementTypes.Walk)
         {
-            GlobalPosition += direction;
+            if (!ParentScene.IsInBoundaries(GlobalPosition + direction))
+                GlobalPosition += direction;
         }
 
         return true;
